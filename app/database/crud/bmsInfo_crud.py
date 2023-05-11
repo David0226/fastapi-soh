709e35cd7ebef3d0b45ac47b206c0008cb2a7af2
David0226 <sorwd2001@naver.com>
2023-05-11 15:04:36 +0900

    error fix

--- app/database/crud/bmsInfo_crud.py
+++ app/database/crud/bmsInfo_crud.py
@@ -1,14 +1,14 @@
 # user table에 대한 필요한 crud를 정의하는 파일
 
 from sqlalchemy.orm import Session
-from ..model import bms_info, bmsInfo_schema
+from ..model import bmsInfo, bmsInfo_schema
 from datetime import date
 
 # Bms id 조건 조회
 def get_bmsInfo(db: Session, bms_id: int):
-    return db.query(bms_info.BmsInfo).filter(bms_info.BmsInfo.bmsId == bms_id).first()
+    return db.query(bmsInfo.BmsInfo).filter(bmsInfo.BmsInfo.bmsId == bms_id).first()
 
 # Bms id와 등록날짜 조건 조회
 def get_bmsInfo_by_date(db: Session, bms_id: int, reg_date: date):
-    return db.query(bms_info.BmsInfo).filter(bms_info.BmsInfo.bmsId == bms_id).filter(bms_info.BmsInfo.regDate == reg_date).first()
+    return db.query(bmsInfo.BmsInfo).filter(bmsInfo.BmsInfo.bmsId == bms_id).filter(bmsInfo.BmsInfo.regDate == reg_date).first()
 
--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
-from router import user_router
+from router import user_router, bmsInfo_router
 
 app = FastAPI()
 
 app.include_router(user_router.router)
+app.include_router(bmsInfo_router.router)
 
 @app.get("/")
 def root():
